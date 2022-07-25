508ff049eb348021438c8f4c0e0535e9cc954778
mova <mova@users.noreply.github.com>
2022-07-25 13:46:06 +0200

    remove logger setup

--- queueflow/logger.py
+++ queueflow/logger.py
@@ -1,23 +1,3 @@
 import logging
-from typing import Optional
 
-from multiprocessing_logging import install_mp_handler
-from rich.highlighter import NullHighlighter
-from rich.logging import RichHandler
-from tqdm.contrib.logging import logging_redirect_tqdm
-
-logger = logging.getLogger(__name__)
-
-
-def setup_logger(
-    filename: Optional[str] = None, print_bool: bool = True, debug: bool = False
-):
-    if filename is not None:
-        filehandler = logging.FileHandler(filename=filename, mode="w")
-        logger.addHandler(filehandler)
-
-    if print_bool:
-        streamhandler = RichHandler(
-            log_time_format="%y-%m-%d %H:%M", highlighter=NullHighlighter()
-        )
-        logger.addHandler(streamhandler)
+logger = logging.getLogger("queueflow")
